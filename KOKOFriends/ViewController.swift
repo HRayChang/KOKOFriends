1e69269b81b8dadd8ced819ebcc26d2824d69786
RayChang <raychang861205@gmail.com>
2024-05-24 16:03:55 +0800

    start KOKOFriends project

---
+++ KOKOFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KOKOFriends
+//
+//  Created by Ray Chang on 2024/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
